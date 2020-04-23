72d21445345b1074883beeaf1860361b41ad0bb7
smartlizzard <pradhanraghunath91@gmail.com>
2020-04-23 22:08:26 +0530

    Update user_data.sh

--- vm/user_data.sh
+++ vm/user_data.sh
@@ -16,4 +16,5 @@ mv linux-amd64/helm /usr/local/bin/helm
 /bin/mkdir /home/ubuntu/.kube
 az login --identity
 az storage blob download -c terraform -n config -f /home/ubuntu/.kube/config --account-name=insightsinfra
+chown ubuntu. /home/ubuntu/.kube/config
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
